80667162bd1767e5edd4b7ce072964f1be93f22c
radioactive <radioactive@debian7>
2020-10-01 08:33:27 -0700

    update

--- src/btconfig/BTFrame.java
+++ src/btconfig/BTFrame.java
@@ -1168,8 +1168,8 @@ long audio_tick_start=0;
 
 
 
-      fw_ver.setText("Latest Avail: FW Date: 202010010808");
-      release_date.setText("Release: 2020-10-01 0808");
+      fw_ver.setText("Latest Avail: FW Date: 202010010826");
+      release_date.setText("Release: 2020-10-01 0826");
       fw_installed.setText("   Installed FW: ");
 
       setProgress(-1);
