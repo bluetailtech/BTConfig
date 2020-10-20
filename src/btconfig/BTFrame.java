9055ed87192afeef23332b42546231cc1a68b382
radioactive <radioactive@debian7>
2020-10-20 05:02:40 -0700

    release 2020-10-19_1333 add end-of-call detection, improve VHF performance

--- src/btconfig/BTFrame.java
+++ src/btconfig/BTFrame.java
@@ -1176,8 +1176,8 @@ int command_input_timeout=0;
 
 
 
-      fw_ver.setText("Latest Avail: FW Date: 202010190632");
-      release_date.setText("Release: 2020-10-19 0632");
+      fw_ver.setText("Latest Avail: FW Date: 202010191333");
+      release_date.setText("Release: 2020-10-19 1333");
       fw_installed.setText("   Installed FW: ");
 
       setProgress(-1);
