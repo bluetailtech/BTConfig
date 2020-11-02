20b787d65afabb9b72c72ab60c14dded7eb19413
radioactive <radioactive@debian7>
2020-11-02 07:04:43 -0800

    release 2020-11-02_0659

--- src/btconfig/BTFrame.java
+++ src/btconfig/BTFrame.java
@@ -1190,7 +1190,7 @@ int command_input_timeout=0;
 
 
       fw_ver.setText("Latest Avail: FW Date: 202011010907");
-      release_date.setText("Release: 2020-11-01 0907");
+      release_date.setText("Release: 2020-11-02 0659");
       fw_installed.setText("   Installed FW: ");
 
       setProgress(-1);
