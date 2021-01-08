53fd0bbb4b94e959b31389bd358f1511d8adf915
radioactive <radioactive@debian7>
2021-01-07 16:30:38 -0800

    release 2021-01-07_1508 add fast scanning support for up to 16 persistent control channels

--- src/btconfig/BTFrame.java
+++ src/btconfig/BTFrame.java
@@ -1308,8 +1308,8 @@ long wdog_time=0;
 
 
 
-      fw_ver.setText("Latest Avail: FW Date: 202101071059");
-      release_date.setText("Release: 2021-01-07 1059");
+      fw_ver.setText("Latest Avail: FW Date: 202101071508");
+      release_date.setText("Release: 2021-01-07 1508");
       fw_installed.setText("   Installed FW: ");
 
       setProgress(-1);
