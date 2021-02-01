bd6f7d1cbfc166a07c2eb7136d3b81091799c703
tvelliott <tvelliott@gmail.com>
2021-01-31 17:07:29 -0800

    release 2021-01-31_recovery_firmware

--- src/btconfig/BTFrame.java
+++ src/btconfig/BTFrame.java
@@ -1311,8 +1311,8 @@ int sys_info_count=0;
 
 
 
-      fw_ver.setText("Latest Avail: FW Date: 202101311133");
-      release_date.setText("Release: 2021-01-31 1133");
+      fw_ver.setText("Latest Avail: FW Date: 202101311704");
+      release_date.setText("Release: 2021-01-31 1704");
       fw_installed.setText("   Installed FW: ");
 
       setProgress(-1);
