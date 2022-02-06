16e6935e04b060c9b806e3ced47a8b56b58e7335
bluetailtech <bluetailtech@gmail.com>
2022-02-06 10:38:18 -0800

    02-06_1033 add support for wio terminal

--- src/btconfig/BTFrame.java
+++ src/btconfig/BTFrame.java
@@ -1472,8 +1472,8 @@ int demod_type=0;
 
 
 
-      fw_ver.setText("Latest Avail: FW Date: 202202052111");
-      release_date.setText("Release: 2022-02-05 21:11");
+      fw_ver.setText("Latest Avail: FW Date: 202202061033");
+      release_date.setText("Release: 2022-02-06 10:33");
       fw_installed.setText("   Installed FW: ");
 
       setProgress(-1);
